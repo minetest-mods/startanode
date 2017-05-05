d4275c102658870f91b552bbd647bc1e133df36e
Alexander Weber <web.alexander@web.de>
2017-05-05 16:19:21 +0200

    removed undernull, new hook after_place_func, rework the waiting the spawn place is generated

--- init.lua
+++ init.lua
@@ -1,36 +1,69 @@
+--[[
+Configuration interface.
+  The values can be changed from other mods at init time
+  Just be sure this mod is in dependency of the other mod
+]]
+
 singlenode = {
 	min_pos = {x = -500, y = -30, z = -500 },
 	max_pos = {x =  500, y =  30, z =  500 },
-	node_name = "mapgen_stone" -- "default:stone"
+	node_name = "mapgen_stone", -- "default:stone"
+	after_place_func = nil, -- function(player, pos) called after the node is placed
+	singlenode_mode = true -- no effect from other mods. Changed too late
 }
 
 -- support for undernull water
-if minetest.get_modpath("water") then
-	minetest.log("[singlenode] compatibility mode to undernull water")
-	local water_level = minetest.setting_get("water_level") or 0
-	singlenode.min_pos.y = water_level
-	singlenode.max_pos.y = water_level
-else
-	minetest.register_on_mapgen_init(function(mgparams)
+minetest.register_on_mapgen_init(function(mgparams)
+	if singlenode.singlenode_mode then
 		minetest.set_mapgen_params({mgname="singlenode"})
-	end)
-end
+	end
+end)
 
-minetest.register_on_newplayer(function(player)
+
+
+local function spawn_node(player, pos)
+
+	-- select new pos if no given
+	if not pos then
+		pos = {
+			x=math.random(singlenode.min_pos.x, singlenode.max_pos.x),
+			y=math.random(singlenode.min_pos.y, singlenode.max_pos.y),
+			z=math.random(singlenode.min_pos.z, singlenode.max_pos.z)
+		}
+	end
+
+	-- Check if spawn place generated and loaded. Force it
+	local node = minetest.get_node_or_nil(pos)
+	if not node or node.name == "ignore" then
+		-- Load the map at pos and try again
+		minetest.emerge_area(pos, pos)
+--		minetest.get_voxel_manip(pos, pos)
+		minetest.after(0.5, spawn_node, player, pos)
+		return
+	end
+
+	-- check if placing allowed at this position
+	if node.is_ground_content == false then
+		minetest.after(0, spawn_node, player, nil) --new position
+	end
+
 	local playername = player:get_player_name()
-	local pos = {
-		x=math.random(singlenode.min_pos.x, singlenode.max_pos.x), 
-		y=math.random(singlenode.min_pos.y, singlenode.max_pos.y),
-		z=math.random(singlenode.min_pos.z, singlenode.max_pos.z)
-	}
-
-	minetest.log("Player "..playername.." Spawned on singlenode at "..minetest.pos_to_string(pos))
-	minetest.get_voxel_manip(pos, pos)
+	minetest.log("Player "..playername.." Node and player spawned at "..minetest.pos_to_string(pos)..". replacing "..node.name)
 	minetest.set_node(pos, {name=singlenode.node_name})
 	player:setpos({x=pos.x,y=pos.y+2,z=pos.z})
 
 	if minetest.global_exists("sethome") then
 		sethome.set(playername, {x=pos.x,y=pos.y+2,z=pos.z})
 	end
-end)
 
+	if singlenode.after_place_func then
+		singlenode.after_place_func(player, pos)
+	end
+
+	-- trigger additional actions like filling chest
+	minetest.forceload_free_block(pos)
+end
+
+minetest.register_on_newplayer(function(player)
+	spawn_node(player, nil)
+end)
